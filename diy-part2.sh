02e984598c565163e0e574b90743fbaa60175bd5
iscodeeasyornot <73045183+iscodeeasyornot@users.noreply.github.com>
2022-12-27 10:46:09 +0800

    edit

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,8 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
+#wifi固件
+mkdir -p target/linux/rockchip/armv8/base-files/lib/firmware/brcm
+cp  ../PATCH/target_king3399_wifi/* target/linux/rockchip/armv8/base-files/lib/firmware/brcm
