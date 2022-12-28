4f5d4b2fb4deb72eeb43df804c9f88b783999ad2
iscodeeasyornot <73045183+iscodeeasyornot@users.noreply.github.com>
2022-12-28 16:39:22 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,10 +15,9 @@ sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_genera
 
 #wifi固件
 mkdir -p target/linux/rockchip/armv8/base-files/lib/firmware/brcm
+mkdir -p package/base-files/lib/firmware/brcm
 cp  ../brcm/* target/linux/rockchip/armv8/base-files/lib/firmware/brcm
-
-wget -P target/linux/rockchip/armv8/base-files/etc/init.d/ https://github.com/friendlyarm/friendlywrt/raw/master-v19.07.1/target/linux/rockchip-rk3399/base-files/etc/init.d/fa-rk3399-pwmfan
-wget -P target/linux/rockchip/armv8/base-files/usr/bin/ https://github.com/friendlyarm/friendlywrt/raw/master-v19.07.1/target/linux/rockchip-rk3399/base-files/usr/bin/start-rk3399-pwm-fan.sh
+cp  ../brcm/* package/base-files/lib/firmware/brcm
 
 # 使用特定的优化
 sed -i 's,-mcpu=generic,-mcpu=cortex-a72.cortex-a53+crypto,g' include/target.mk
